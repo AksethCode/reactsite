3ee5c3ca0027085bb963e93b376c3e13072d7389
Akshay Seth <akseth.office@gmail.com>
2023-12-24 19:17:33 +0530

    adding some components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
+import Heading from './components/Heading';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
     </div>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,7 @@
+function Heading(){
+    return (
+        <h1>This is Heading</h1>
+    )
+}
+
+export default Heading;
